8e1646bd59c9eaeca78c578cce5c81d8bc4b2787
dhermaglos <111802099+dhermaglos@users.noreply.github.com>
2024-10-21 18:02:57 -0300

    Update home.dart

--- lib/presentation/screens/home.dart
+++ lib/presentation/screens/home.dart
@@ -28,15 +28,14 @@ class _HomeState extends State<Home> {
     Text('Perfil'), // Página de perfil
   ];
 
-
-   @override
+  @override
   Widget build(BuildContext context) {
     // Obtén la altura de la pantalla
     double screenHeight = MediaQuery.of(context).size.height;
 
     // Establece minHeight y maxHeight como un porcentaje de la altura de la pantalla
-    double minHeight = screenHeight * 0.4; // 40% de la altura de la pantalla
-    double maxHeight = screenHeight * 0.8; // 80% de la altura de la pantalla
+    double minHeight = screenHeight * 0.44; // 40% de la altura de la pantalla
+    double maxHeight = screenHeight * 0.88; // 80% de la altura de la pantalla
 
     return Scaffold(
       appBar: AppBar(
@@ -61,7 +60,8 @@ class _HomeState extends State<Home> {
           children: [
             _Header(), // Agrega tu header aquí
             Expanded(
-              child: _widgetOptions[_selectedIndex], // Cambia el contenido según el índice seleccionado
+              child: _widgetOptions[
+                  _selectedIndex], // Cambia el contenido según el índice seleccionado
             ),
           ],
         ),
@@ -73,7 +73,6 @@ class _HomeState extends State<Home> {
     );
   }
 }
-
 
 // Widget de encabezado que contiene el saludo, foto y subtítulo
 class _Header extends StatelessWidget {
@@ -89,7 +88,8 @@ class _Header extends StatelessWidget {
           // Foto de perfil
           const CircleAvatar(
             radius: 30,
-            backgroundImage: NetworkImage('https://example.com/tu-foto.jpg'), // Reemplaza con la URL de tu foto
+            backgroundImage: NetworkImage(
+                'https://example.com/tu-foto.jpg'), // Reemplaza con la URL de tu foto
           ),
           const SizedBox(width: 16), // Espacio entre la foto y el texto
           Expanded(
@@ -122,218 +122,230 @@ class _Header extends StatelessWidget {
 class _BodyView extends StatelessWidget {
   const _BodyView();
 
+
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      // Primer carrusel con 6 botones
-      CarouselSlider(
-        options: CarouselOptions(
-          height: 150.0, // Altura del carrusel
-          autoPlay: false,
-          enlargeCenterPage: true,
-          viewportFraction: 0.9,
-          enableInfiniteScroll: false,
+    return Column(
+      children: [
+        // Primer carrusel con 6 botones
+        LayoutBuilder(
+          builder: (context, constraints) {
+                double screenHeight = MediaQuery.of(context).size.height;
+
+            double carouselHeight = screenHeight *0.2; // Usa el maxHeight disponible del LayoutBuilder
+
+           
+            double height1 = carouselHeight * 0.6;
+            double width1 = carouselHeight * 0.88; // 88% de la altura disponible
+
+            return CarouselSlider(
+              options: CarouselOptions(
+                height: carouselHeight, // Usa minHeight para la altura del carrusel
+                autoPlay: false,
+                enlargeCenterPage: true,
+                viewportFraction: 0.9,
+                enableInfiniteScroll: false,
+              ),
+              items: [
+                // Primera slide con 3 botones
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SizedBox(
+                      height: height1,
+                      width: 120,
+                      child: Stack(
+                        alignment: Alignment.topCenter, // Centra el contenido en la parte superior
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              context.go('/meals_registration');
+                            },
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20), // Bordes redondeados
+                              ),
+                            ),
+                            child: const Column(
+                              mainAxisAlignment: MainAxisAlignment.end, // Alinea los elementos al final
+                              children: [
+                                SizedBox(height: 8), // Espacio superior para centrar el texto
+                                Text(
+                                  'Agregar Comida',
+                                  textAlign: TextAlign.center, // Alinea el texto al centro
+                                ),
+                              ],
+                            ),
+                          ),
+                          Positioned(
+                            top: height1 * 0.1, // Ajusta este valor para posicionar el '+' donde lo necesites
+                            child: Container(
+                              width: height1 * 0.5, // Ajusta el tamaño según sea necesario
+                              height: height1 * 0.5, // Ajusta el tamaño según sea necesario
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.white, // Color de fondo del círculo
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(0.2),
+                                    offset: Offset(0, 2),
+                                    blurRadius: 4,
+                                  ),
+                                ],
+                              ),
+                              child: const Center(
+                                child: Text(
+                                  '+',
+                                  style: TextStyle(
+                                    fontSize: 18, // Ajusta el tamaño del texto
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black, // Color del texto
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 100,
+                      width: 100,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          context.go('/meals_list');
+                        },
+                        child: const Text('Botón 2'),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 100,
+                      width: 100,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          context.go('/meals_list');
+                        },
+                        child: const Text('Botón 3'),
+                      ),
+                    ),
+                  ],
+                ),
+                // Segunda slide con 3 botones
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SizedBox(
+                      height: 100,
+                      width: 100,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          context.go('/meals_list');
+                        },
+                        child: const Text('Botón 4'),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 100,
+                      width: 100,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          context.go('/meals_list');
+                        },
+                        child: const Text('Botón 5'),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 100,
+                      width: 100,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          context.go('/meals_list');
+                        },
+                        child: const Text('Ver Historial'),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            );
+          },
         ),
-        items: [
-          // Primera slide con 3 botones
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-             SizedBox(
-  height: 100,
-  width: 120,
-  child: Stack(
-    alignment: Alignment.topCenter, // Centra el contenido en la parte superior
-    children: [
-      ElevatedButton(
-        onPressed: () {
-          context.go('/meals_registration');
-        },
-        style: ElevatedButton.styleFrom(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20), // Bordes redondeados
+
+        // Segundo carrusel con un botón y un rectángulo redondeado
+        CarouselSlider(
+          options: CarouselOptions(
+            height: 75.0, // Altura del carrusel reducida
+            autoPlay: false,
+            enlargeCenterPage: true,
+            viewportFraction: 0.9,
+            enableInfiniteScroll: false,
           ),
-        ),
-        child: const Column(
-          mainAxisAlignment: MainAxisAlignment.end, // Alinea los elementos al final
-          children: [
-            SizedBox(height: 8), // Espacio superior para centrar el texto
-            Text(
-              'Agregar Comida',
-              textAlign: TextAlign.center, // Alinea el texto al centro
+          items: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                  height: 60,
+                  width: 100,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      context.go('/exercise_registration');
+                    },
+                    child: const Text('Añadir Ejercicio'),
+                  ),
+                ),
+                SizedBox(
+                  height: 60,
+                  width: 220,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    alignment: Alignment.center,
+                    child: const Text(
+                      '80% de Metas Alcanzadas',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                  height: 60,
+                  width: 220,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    alignment: Alignment.center,
+                    child: const Text(
+                      '80% de Metas Alcanzadas',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 60,
+                  width: 100,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      context.go('/exercises_list');
+                    },
+                    child: const Text('Ver Historial'),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
-      ),
-      Positioned(
-        top: -10, // Ajusta este valor para posicionar el '+' donde lo necesites
-        child: Container(
-          width: 24, // Ajusta el tamaño según sea necesario
-          height: 24, // Ajusta el tamaño según sea necesario
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: Colors.white, // Color de fondo del círculo
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.2),
-                offset: Offset(0, 2),
-                blurRadius: 4,
-              ),
-            ],
-          ),
-          child: const Center(
-            child: Text(
-              '+',
-              style: TextStyle(
-                fontSize: 18, // Ajusta el tamaño del texto
-                fontWeight: FontWeight.bold,
-                color: Colors.black, // Color del texto
-              ),
-            ),
-          ),
-        ),
-      ),
-    ],
-  ),
-),
-SizedBox(
-  height: 100,
-  width: 100,
-  child: ElevatedButton(
-    onPressed: () {
-      context.go('/meals_list');
-    },
-    child: const Text('Botón 2'),
-  ),
-),
-SizedBox(
-  height: 100,
-  width: 100,
-  child: ElevatedButton(
-    onPressed: () {
-      context.go('/meals_list');
-    },
-    child: const Text('Botón 3'),
-  ),
-),
-            ],
-          ),
-          // Segunda slide con 3 botones
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              SizedBox(
-                height: 100,
-                width: 100,
-                child: ElevatedButton(
-                  onPressed: () {
-                    context.go('/meals_list');
-                  },
-                  child: const Text('Botón 4'),
-                ),
-              ),
-              SizedBox(
-                height: 100,
-                width: 100,
-                child: ElevatedButton(
-                  onPressed: () {
-                    context.go('/meals_list');
-                  },
-                  child: const Text('Botón 5'),
-                ),
-              ),
-              SizedBox(
-                height: 100,
-                width: 100,
-                child: ElevatedButton(
-                  onPressed: () {
-                    context.go('/meals_list');
-                  },
-                  child: const Text('Ver Historial'),
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
-
-
-
-      
-      // Segundo carrusel con un botón y un rectángulo redondeado
-      CarouselSlider(
-        options: CarouselOptions(
-          height: 75.0, // Altura del carrusel reducida
-          autoPlay: false,
-          enlargeCenterPage: true,
-          viewportFraction: 0.9,
-          enableInfiniteScroll: false,
-        ),
-        items: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              SizedBox(
-                height: 60,
-                width: 100,
-                child: ElevatedButton(
-                  onPressed: () {
-                    context.go('/exercise_registration');
-                  },
-                  child: const Text('Añadir Ejercicio'),
-                ),
-              ),
-              SizedBox(
-                height: 60,
-                width: 220,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.amber,
-                    borderRadius: BorderRadius.circular(30.0),
-                  ),
-                  alignment: Alignment.center,
-                  child: const Text(
-                    '80% de Metas Alcanzadas',
-                    style: TextStyle(color: Colors.black),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              SizedBox(
-                height: 60,
-                width: 220,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.amber,
-                    borderRadius: BorderRadius.circular(30.0),
-                  ),
-                  alignment: Alignment.center,
-                  child: const Text(
-                    '80% de Metas Alcanzadas',
-                    style: TextStyle(color: Colors.black),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 60,
-                width: 100,
-                child: ElevatedButton(
-                  onPressed: () {
-                    context.go('/exercises_list');
-                  },
-                  child: const Text('Ver Historial'),
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
-      const Text("Calorías x día"),
-    ]);
+        const Text("Calorías x día"),
+      ],
+    );
   }
 }
 
@@ -352,10 +364,10 @@ class _Graph extends ConsumerWidget {
         x: index,
         barRods: [
           BarChartRodData(
-            toY: meal.protein, 
+            toY: meal.protein,
             color: Colors.amber,
-            width: 8, 
-            borderRadius: BorderRadius.zero, 
+            width: 8,
+            borderRadius: BorderRadius.zero,
           )
         ],
       );
@@ -376,7 +388,8 @@ class _Graph extends ConsumerWidget {
                   showTitles: true,
                   getTitlesWidget: (double value, TitleMeta meta) {
                     final int index = value.toInt();
-                    if (index < 0 || index >= meals.length) return const Text('');
+                    if (index < 0 || index >= meals.length)
+                      return const Text('');
 
                     final String date = meals[index].dateTime.toString();
                     if (shownDates.contains(date)) return const Text('');
