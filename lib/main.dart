c0cbd12207e4061241a237ba4298bf136cf8139a
Vartencito <104112059+Vartencito@users.noreply.github.com>
2024-10-21 18:28:23 -0300

    agregadas las dependencias de firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:fittracker/firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fittracker/core/router/app_router.dart';
 
 void main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
+  WidgetsFlutterBinding.ensureInitialized();
+  if (Firebase.apps.isEmpty) {
+    await Firebase.initializeApp();
+  }
 
   runApp(const ProviderScope(
     child: MainApp(),
